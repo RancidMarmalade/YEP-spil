2499e489aefb3b12fdc6d9c855c5f1df14bf80b1
davidala1 <78424061+davidala1@users.noreply.github.com>
2021-03-16 14:23:01 +0100

    Yee

--- Evolution spil/Assets/scripts/blue/RandomMovementBlue.cs
+++ Evolution spil/Assets/scripts/blue/RandomMovementBlue.cs
@@ -7,21 +7,25 @@ public class RandomMovementBlue : MonoBehaviour
     public Rigidbody Rb;
     public Vector3 Destination;
     public float Distance;
+    private int Speed;
 
     public void Start()
     {
         Destination = new Vector3(Random.Range(-9, 9), 0, Random.Range(-4, 4));
+        Speed = PlayerPrefs.GetInt("SpeedBlue");
     }
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Rb.drag = Rb.velocity.magnitude / 2;
         transform.LookAt(Destination);
-        Rb.AddRelativeForce(0, 0, 5);
+        Rb.AddRelativeForce(0, 0, Speed);
         if (Vector3.Distance(transform.position, Destination) <= 1)
         {
             Destination = new Vector3(Random.Range(-9, 9), 0, Random.Range(-4, 4));
             Debug.Log(" " + Destination);
         }
     }
+
+
 }
--- Evolution spil/Assets/scripts/red/RandomMovementRed.cs
+++ Evolution spil/Assets/scripts/red/RandomMovementRed.cs
@@ -7,17 +7,18 @@ public class RandomMovementRed : MonoBehaviour
     public Rigidbody Rb;
     public Vector3 Destination;
     public float Distance;
-
+    public int Speed;
     public void Start()
     {
         Destination = new Vector3(Random.Range(-9, 9),0, Random.Range(-4, 4));
+        Speed = PlayerPrefs.GetInt("SpeedRed");
     }
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Rb.drag = Rb.velocity.magnitude / 2;
         transform.LookAt(Destination);
-        Rb.AddRelativeForce(0,0,5);
+        Rb.AddRelativeForce(0,0,Speed);
         if (Vector3.Distance(transform.position, Destination) <= 1)
         {
             Destination = new Vector3(Random.Range(-9, 9),0, Random.Range(-4, 4));
